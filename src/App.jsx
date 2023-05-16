6e994c0cfca5d2d062978d1d918d7deffedc1f36
Reimon Odakura <pwd2mmi2ig@yahoo.co.jp>
2023-05-16 19:01:21 +0000

    Update 3 files

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,33 @@ import React from "react";
 import "./styles.css";
 
 export const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <div className="input">
+        <input placeholder="TODOを入力" />
+        <button>追加</button>
+      </div>
+
+      <div className="incomplate">
+        <p className="title">未完了のTODO</p>
+        <ul>
+          <div className="one_line">
+            <li>aaaa</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+        </ul>
+      </div>
+
+      <div className="complate">
+        <p className="title">未完了のTODO</p>
+        <ul>
+          <div className="one_line">
+            <li>aaaa</li>
+            <button>戻る</button>
+          </div>
+        </ul>
+      </div>
+    </>
+  );
 };
